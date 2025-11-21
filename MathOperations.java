adf3ae812a3cbfd00c9d89742830157a97213063
Bishesta Shakya <bishestasakya@gmail.com>
2025-11-21 13:02:53 +0545

    Initial sharing of project

---
+++ MathOperations.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class MathOperations
+{
+    public static void main(String[] args){
+        //Arithmetic Operator
+        int a=5; 
+        int b=8;
+        System.out.println("Sum= "+(a+b));
+        System.out.println("Difference= "+(a-b));
+        System.out.println("Multiple= "+(a*b));
+        System.out.println("Division= "+(a/b));
+        System.out.println("Remainder= "+(a%b));
+        
+        //Unary Operator
+        int h=1;
+        System.out.println(h); 
+        System.out.println(++h); 
+        System.out.println(h); 
+        System.out.println(h++); 
+        System.out.println(h);
+        
+        //Assignment Operator
+        int x= 5;
+        x+=5;
+        System.out.println(x);
+        
+        //Relational Operator
+        int p=10, q=20;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        
+        //Logical Operator
+        int marks= 80;
+        if (marks<40){
+        System.out.println("Student is fail.");
+        }
+        
+        //Ternary Operator
+        Scanner scan= new Scanner(System.in);
+        int age= scan.nextInt();
+        System.out.println("Enter your age:"+ age);
+        String canDrive=(age>=18)?"You can drive.":"You can't drive";
+        System.out.println(canDrive);
+    }
+}
